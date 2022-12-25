074c3daa37913de71ab3e1cabb5b0a6b1eb84384
cemcoe <chemxcode@gmail.com>
2022-12-25 11:56:55 +0800

    feat: first echarts

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,43 @@
+<script setup lang="ts">
+import * as echarts from "echarts";
+import { onMounted } from "vue";
+
+// 基于准备好的dom，初始化echarts实例
+onMounted(() => {
+  const myChart = echarts.init(
+    document.getElementById("myChart") as HTMLElement
+  );
+
+  console.log(myChart);
+
+  // 绘制图表
+  myChart.setOption({
+    title: {
+      text: "ECharts 入门示例",
+    },
+    tooltip: {},
+    xAxis: {
+      data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+    },
+    yAxis: {},
+    series: [
+      {
+        name: "销量",
+        type: "bar",
+        data: [5, 20, 36, 10, 10, 20],
+      },
+    ],
+  });
+});
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div id="myChart"></div>
 </template>
 
-<style></style>
+<style scoped>
+#myChart {
+  width: 400px;
+  height: 200px;
+}
+</style>
