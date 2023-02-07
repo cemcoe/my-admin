86e0b221e1a272952c55544fd36ff31acb62e5c5
cemcoe <chemxcode@gmail.com>
2023-02-07 12:19:30 +0800

    feat: read packagejson and display on analysisOverview

--- src/views/AnalysisOverview/AnalysisOverview.vue
+++ src/views/AnalysisOverview/AnalysisOverview.vue
@@ -1,14 +1,67 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import packageData from "../../../package.json";
+
+const { dependencies, devDependencies } = packageData;
+
+function objectToArray(object: Object) {
+  return Object.keys(object).map((key) => {
+    //stackoverflow.com/questions/57086672/element-implicitly-has-an-any-type-because-expression-of-type-string-cant-b
+    return {
+      name: key,
+      version: object[key as keyof typeof object],
+    };
+  });
+}
+
+const dependenciesArray = objectToArray(dependencies);
+
+const devDependenciesArray = objectToArray(devDependencies);
+</script>
 
 <template>
   <div class="">
-    <h2>核心技术</h2>
-    <ul>
-      <li>Vue3 + Vue-Router + Pinia + Element-Plus + Vite + TypeScript</li>
-      <li>页面级别组件</li>
-      <li>通过配置生成页面</li>
-    </ul>
+    <el-card class="info" style="margin-bottom: 20px">
+      admin-template 是一个基于 Vue3 + Vue-Router + Pinia + Element-Plus + Vite
+      + TypeScript 的管理系统模板。
+    </el-card>
+
+    <div class="container">
+      <el-card class="box-card">
+        <template #header>
+          <div class="card-header">
+            <span>生产环境依赖</span>
+          </div>
+        </template>
+        <el-table :data="dependenciesArray" style="width: 100%">
+          <el-table-column prop="name" label="名称" width="180" />
+          <el-table-column prop="version" label="版本" />
+        </el-table>
+      </el-card>
+
+      <el-card class="box-card">
+        <template #header>
+          <div class="card-header">
+            <span>开发环境依赖</span>
+          </div>
+        </template>
+        <el-table :data="devDependenciesArray" style="width: 100%">
+          <el-table-column prop="name" label="名称" width="180" />
+          <el-table-column prop="version" label="版本" />
+        </el-table>
+      </el-card>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.info {
+  line-height: 30px;
+}
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+.box-card {
+  width: 480px;
+}
+</style>
